87d1f199801a67bb1e9094aacf24d1ceb604eeed
KAMRAN <KAMRAN@DESKTOP-K1BV6A4>
2024-03-20 05:19:45 +0330

    Add project files.

---
+++ Reciver_Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Reciver_Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = RabbitMQConfig.Instance.CreateConnection();
+            var channel = RabbitMQConfig.Instance.CreateModel(connection);
+            channel.QueueDeclare("myQueue01", true, false, true, null);
+
+            #region BasicQos
+            channel.BasicQos(0, 1, false);//Fair dispatch
+            //کار ها به صورت تکی به هر کدام از سرور ها ارسال میشود تا بعد از 
+            //انجام کارشان کار دیگری به ان ها داده شود
+            //با این کار سربار بر اساس قدرت هر سرور تقسیم میشود
+            //تا در صورت سرعتی تر بودن یک سرور کار های بیشتری به او سپرده شود
+            #endregion
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, eventArg) =>
+            {
+                var body = eventArg.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received Message " + message);
+
+                channel.BasicAck(eventArg.DeliveryTag, true);//با این دستور میتوانید به صورت دستی مشخص کنید که دیتا دریافت شده و هندل نیز شده و شناسه پیغام را به
+                // ارسال میکنید تا آن را از صف حذف کندRabbit
+            };
+            channel.BasicConsume("myQueue01", false, consumer);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sender_Producer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+namespace Sender_Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = RabbitMQConfig.Instance.CreateConnection();
+            var channel = RabbitMQConfig.Instance.CreateModel(connection);
+
+            //ایجاد یک صف
+            channel.QueueDeclare("myQueue01", true, false, true, null);
+            //QueueDeclare پارامتر های متد 
+            //1==>نام صف
+            //2==durable==>بودن صف در هارد ذخیره میشود تا در صورت استاپ شدن سرویس ربیت صف در هارد نگهداری شود و بعد از فعال سازی سرویس دوباره صف  اضافه شود true در صورت
+            //3==exvlusive==> Consumer حذف صف بعد از بسته شدن کانکشن 
+            //4==autoDelete==>صف به صورت خودکار حذف میشود
+            for (int i = 0; i < 3; i++)
+            {
+                var body = Encoding.UTF8.GetBytes($"Test1 {i}");
+
+                #region Persistent
+                //  RabbitMQ با این کار اطلاعات موجود در صف را در هارد نگهداری میکنیم تا درصورتی که سرویس
+                // به هر دلیلی استاپ شد دیتا ها در هارد نگه داری شوند.
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                #endregion
+
+
+                channel.BasicPublish("", "myQueue01", properties, body);
+            }
+
+            //باید دیتا را به بایت تبدیل کنیم برای ارسال
+
+            //پیغام به این روش ارسال میشود
+
+            channel.Close();
+            connection.Close();
+            Console.ReadKey();
+        }
+    }
+}
